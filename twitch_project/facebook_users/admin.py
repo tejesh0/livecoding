0752a943bdb70e95cf6816aec3f628b724aaf757
tejesh95 <tejeshpapineni95@gmail.com>
2016-05-26 00:31:52 +0530

    create admin interface for facebook group members

--- twitch_project/facebook_users/admin.py
+++ twitch_project/facebook_users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import GroupMember
 
 # Register your models here.
+
+
+class GroupMemberAdmin(admin.ModelAdmin):
+    list_display = ('profile_id', 'name', 'is_admin')
+
+admin.site.register(GroupMember, GroupMemberAdmin)
