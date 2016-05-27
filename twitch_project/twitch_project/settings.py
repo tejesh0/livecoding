74b74fd3bcb7e2e5a5b1e837b8857b6539159878
tejesh95 <tejeshpapineni95@gmail.com>
2016-05-27 19:09:59 +0530

    prod settings

--- twitch_project/twitch_project/settings.py
+++ twitch_project/twitch_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '3uti8@)jr%tsj(sn@h17+8(2i7dw^3vn-w3pv-!a2kk)vzw)_6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
